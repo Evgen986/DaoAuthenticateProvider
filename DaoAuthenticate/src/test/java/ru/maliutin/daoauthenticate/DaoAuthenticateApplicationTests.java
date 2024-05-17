fd075306fe75e5d1ea83023b7e0edbd8fa755aff
EvgenyMaliutin <evgen986@mail.ru>
2024-05-17 15:13:25 +0300

    initial commit

---
+++ DaoAuthenticate/src/test/java/ru/maliutin/daoauthenticate/DaoAuthenticateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maliutin.daoauthenticate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoAuthenticateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
